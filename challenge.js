649f18562367eb13279160ee9108e6a683e7b1fd
Sam Bateman <sam.bateman@holidayextras.com>
2019-09-06 09:50:44 +0100

    my solution?

--- challenge.js
+++ challenge.js
@@ -1,5 +1,48 @@
+const getAscendingAndDescendingInts = (number) => {
+  let parts = []
+
+  while (number) {
+    parts.push(number % 10);
+    number = Math.floor(number / 10);
+  }
+
+  if (parts.length < 4) {
+    parts.length = 4
+    parts = Array.from(parts, p => p || 0)
+  }
+
+  let ascMultiplier = 1000
+  let descMultiplier = 1000
+  let ascParts = parts.sort()
+  let descParts = [].concat(ascParts).reverse()
+
+  const asc = ascParts.reduce((total, part) => {
+    total += (part * ascMultiplier)
+    ascMultiplier /= 10
+    return total
+  }, 0)
+
+  let desc = descParts.reduce((total, part) => {
+    total += (part * descMultiplier)
+    descMultiplier /= 10
+    return total
+  }, 0)
+
+  return { asc, desc }
+}
+
 const kaprekarsConstantSteps = number => {
-  throw new Error('implement me!')
+  let totalIterations = 0;
+  let ascDescObj;
+  let result = number;
+
+  while (result !== 6174) {
+    totalIterations++
+    ascDescObj = getAscendingAndDescendingInts(result)
+    result = ascDescObj.desc - ascDescObj.asc
+  }
+
+  return totalIterations
 }
 
 module.exports = { kaprekarsConstantSteps }
