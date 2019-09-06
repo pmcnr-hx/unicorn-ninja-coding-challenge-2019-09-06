a24295c936ad40a0cae3e1e9569cc23e96f651de
Pedro Romano <pedro.romano@holidayextras.com>
2019-09-06 14:00:09 +0100

    feat(challenge.js): numeric solution using generator

--- challenge.js
+++ challenge.js
@@ -1,5 +1,31 @@
-const kaprekarsConstantSteps = number => {
-  throw new Error('implement me!')
+const DIGITS_COUNT = 4
+
+function* digitsGenerator(number) {
+  let remaining = number
+  let count = DIGITS_COUNT
+  while (count--) {
+    yield remaining % 10
+    remaining = Math.floor(remaining / 10)
+  }
+}
+
+const digitsToNumber = digits => {
+  return digits.reverse().reduce((number, digit) => number * 10 + digit, 0)
+}
+
+const sorted = (digits, reverse = false) => {
+  const direction = reverse ? -1 : 1
+  const cmp = (a, b) => direction * (b - a)
+  return digits.sort(cmp)
+}
+
+const kaprekarsConstantSteps = (number, count = 0) => {
+  if (number === 6174) return count
+  const digits = [...digitsGenerator(number)]
+  const subtrahend = digitsToNumber(sorted(digits, true))
+  const minuend = digitsToNumber(sorted(digits))
+  const result = subtrahend - minuend
+  return kaprekarsConstantSteps(result, count + 1)
 }
 
 module.exports = { kaprekarsConstantSteps }
